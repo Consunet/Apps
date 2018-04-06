81687e490158ba52fa5776a079c48c43595eb640
danielkoch86 <danielkoch@consunet.com.au>
2018-04-06 13:01:29 +0930

    Final teaks before pull.

--- EveryPass/test/mocha_test_vars.js
+++ EveryPass/test/mocha_test_vars.js
@@ -3,5 +3,6 @@ const BASE_URL = "http://localhost:8000/public_html/";
 exports.TEST_UNENCRYPTED_URL = BASE_URL + "en/index.html";
 exports.TEST_ENCRYPTED_URL = BASE_URL + "test_encrypted.html";
 exports.TEST_IMPORTED_URL = BASE_URL + "test_imported.html";
+exports.TEST_LOCAL_PAGE = BASE_URL + "local_test_page";
 exports.TEST_PASSWORD = "password";
 exports.TEST_HINT = "hint value";
